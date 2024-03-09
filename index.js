23f60b6c42349bfbe608d6b8c183b66f0d4fe4d5
khalilsikder <khalil.sikder03@gmail.com>
2024-03-09 08:57:15 +0600

    update something

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require ('cors');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 // middleware
@@ -13,6 +13,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// const verifyJWT = (req,res,next) =>{
+// const authorization = req.headers.authorization;
+// if(!authorization){
+//   return res.status(401).send({error:true,message:'unauthorize access'});
+// }
+// const token = authorization.split(' ')[1]
+// jwt.verify(token.process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+//   if(err){
+//    return res.status(401).send({error:true,message:'unauthorize access'})
+//   }
+//   req.decoded = decoded;
+//   next();
+// })
+// }
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.fazwg.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,20 +45,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const cartsCollection = client.db("physical").collection("carts");
     const usersCollection = client.db("physical").collection("users");
+    const cartsCollection = client.db("physical").collection("carts");
 
-    app.get('/users',async(req,res) =>{
-      const result = await usersCollection.findOne().toArray();
-      res.send(result); 
+    app.get('/users',async(req,res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
     })
-
-    app.post('/jwt',(req,res) =>{
-      const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
-      res.send({token})
-    })
-
+    
     app.post('/users',async(req,res) => {
       const user = req.body
       const query = {email: user.email}
@@ -56,42 +64,83 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/users/admin/:id',async(req,res) =>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const updateDoc = {
-      $set: {
-        role: 'admin'
-      },
-    };
-    const result = await usersCollection.updateOne(filter,updateDoc);
-    res.send(result)  
+    app.get('/carts',async(req,res) =>{
+      const result = await cartsCollection.find().toArray();
+      res.send(result); 
     })
 
-    app.get("/carts",async(req,res) =>{
-      const result = await cartsCollection.find().toArray()
-      res.send(result)
-    })
-
-    // save instructor email
-    app.put('/carts/:email', async(req,res) =>{
-      const email = req.params.email
-      const user = req.body
-      const query = {email: email }
-      const options = {upsert: true }
-      const updateDoc = {
-        $set: user
+    app.get("/carts" ,async(req,res) =>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]) 
       }
-      const result = await cartsCollection.updateOne(query,updateDoc,options)
-      console.log(result);
+      const query = {email: email};
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post("/carts",async(req,res) =>{
+      const item = req.body;
+      console.log(item);
+      const result = await cartsCollection.insertOne(item)
       res.send(result)
     })
+
     app.delete('/carts/:id',async(req,res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await cartsCollection.deleteOne(query)
       res.send(result)
     })
+
+    // app.post('/jwt',(req,res) =>{
+    //   const user = req.body;
+    //   const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+    //   res.send({token})
+    // })
+
+    // app.get('/users/admin/:email', async(req,res) =>{
+    //   const email = req.params.email;
+    //   if(req.decoded.email !== email){
+    //     res.send ({admin:false});
+    //   }
+    //   const query = {email: email};
+    //   const user = await usersCollection.findOne(query);
+    //   const result = {admin: user?.role === 'admin'}
+    //   res.send(result);
+    // })
+
+
+
+    // app.patch('/users/admin/:id',async(req,res) =>{
+    // const id = req.params.id;
+    // const filter = {_id: new ObjectId(id)};
+    // const updateDoc = {
+    //   $set: {
+    //     role: 'admin'
+    //   },
+    // };
+    // const result = await usersCollection.updateOne(filter,updateDoc);
+    // res.send(result)  
+    // })
+
+    
+
+    // save instructor email
+    // app.put('/carts/:email', async(req,res) =>{
+    //   const email = req.params.email
+    //   const user = req.body
+    //   const query = {email: email }
+    //   const options = {upsert: true }
+    //   const updateDoc = {
+    //     $set: user
+    //   }
+    //   const result = await cartsCollection.updateOne(query,updateDoc,options)
+    //   console.log(result);
+    //   res.send(result)
+    // })
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
